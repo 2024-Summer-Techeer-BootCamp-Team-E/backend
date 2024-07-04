0b1850191c4bc56bc7a8c6fca33495956de0803d
frozzun <eonjun0783@naver.com>
2024-07-04 14:29:05 +0900

    feat : 상품정보 SELECT 구현

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,7 @@
 from django_backend.urls import path
 from . import views
+from product.ali import product_detail_API
 urlpatterns = [
     path('', views.index),
+    path('product/info/', product_detail_API.as_view(), name = 'info'),
 ]
