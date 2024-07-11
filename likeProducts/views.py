3c930144f7e9c6f3dbfc6b58075ae349abdaa9f3
frozzun <eonjun0783@naver.com>
2024-07-11 16:20:04 +0900

    swagger setting complete

--- likeProducts/views.py
+++ likeProducts/views.py
@@ -3,13 +3,16 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import LikeProduct
-from .serializers import LikeProductSerializer
+from .serializers import (LikeProductSerializer, GetRequestSerializer, GetResponseSerializer, PostRequestSerializer,
+                          PostResponseSerializer, DeleteRequestSerializer)
 from products.models import Product
+from drf_yasg.utils import swagger_auto_schema
 # Create your views here.
 
 class LikeProductView(APIView):
   permission_classes = [IsAuthenticated]
 
+  @swagger_auto_schema(request_body=PostRequestSerializer, responses={"201": PostResponseSerializer})
   def post(self, request):
     try:
       user_id = request.user
@@ -38,8 +41,7 @@ class LikeProductView(APIView):
       return Response({"error": "An unexpected error occurred: " + str(e)},
                       status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-
-
+  @swagger_auto_schema(query_serializer=GetRequestSerializer, responses={"200":GetResponseSerializer})
   def get(self, request): #헤더에 access 토큰 박아야됨
     try:
       # 현재 인증된 사용자의 좋아요한 상품들을 필터링하여 가져옵니다.
@@ -49,6 +51,7 @@ class LikeProductView(APIView):
     except Exception as e:
       return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
+  @swagger_auto_schema(request_body=DeleteRequestSerializer, responses={"204": 'Deleted successfully', "404": 'LikeProduct not found', "400": 'Error'})
   def delete(self, request):
     id = request.data.get('id')
     user_id = request.user
