f98408e2b22e22ad4f5d1a6dd656aeed19aae7a6
sungho jo <tjdgh5295@naver.com>
2024-07-18 21:41:35 +0900

    temp3

--- search/views.py
+++ search/views.py
@@ -3,21 +3,31 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .amazon import Comprehend
+from .models import Search
 
-class TranslateView(APIView):
-    def post(self, request):
-        search_url = request.data['search_url']
-        product_name = request.data['product_name']
+def getKewords(self, request):
+    search_url = request.data['search_url']
 
-        translate_text = Comprehend.translate(product_name)
-        
-        types = ['ORGANIZATION', 'COMMERCIAL_ITEM', 'QUANTITY']
-        keyword_list = []
+    searched_product = Search.objects.filter(search_url=search_url).values('name')
 
-        keyword = Comprehend.entities1(translate_text)
-        
-        for entity in keyword['Entities']:
-            if entity['Type'] in types:
-                keyword_list.append(entity['Text'])
+    translate_text = Comprehend.translate(searched_product)
+            
+    types = ['ORGANIZATION', 'COMMERCIAL_ITEM', 'QUANTITY']
+    keyword_list = []
 
+    keyword = Comprehend.entities1(translate_text)
 
+    #if 'COMMERCIAL_ITEM' not in keyword['Entities']:
+    #    keyword = Comprehend.entities2(translate_text)
+    #    categories_id = 0
+    #    search = Search(
+    #        categories_id = categories_id
+    #        keyword = keyword
+    #    )
+    #    search.save()
+            
+    for entity in keyword['Entities']:
+        if entity['Type'] in types:
+            keyword_list.append(entity['Text'])
+
+    
