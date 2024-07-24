f62744b7526e3060c5f5f636dfe413930cc5de40
sungho jo <tjdgh5295@naver.com>
2024-07-24 13:23:12 +0900

    keyword gpt모델 연결 및 카테고리 아이디 추출

--- search/views.py
+++ search/views.py
@@ -6,9 +6,11 @@ from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
 from .amazon import Comprehend
 from .models import Search
+from .gpt import KeywordExtractor
 from products.models import Product
 from .serializers import KeywordsSerializer
 from .serializers import KeywordRequestSerializer, KeywordResponseSerializer
+import json
 
 def extract_commercial_item_text(data):
     commercial_items = []
@@ -26,58 +28,80 @@ class KeywordView(APIView):
             if not search_url:
                 return Response({"error": "URL is required"}, status=status.HTTP_400_BAD_REQUEST)
             
+            existing_urls = Search.objects.filter(search_url=search_url)
+            if existing_urls.exists():
+                # keyword 값이 null인지 확인
+                null_keywords = existing_urls.filter(keyword__isnull=True)
+                if null_keywords.exists():
+                    pass
+            
         except Exception as e:
             return Response({"Error : ", str(e)}, status=status.HTTP_400_BAD_REQUEST)
         
         product = get_object_or_404(Search, search_url=search_url)
 
-        #name = Search.objects.filter(search_url=search_url).values('name')
-        #price = Search.objects.filter(search_url=search_url).values('price')
-
         try:
-            #name = request.data.get('name')
             if not product.name:
                 return Response({"error": "No name provided"}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({"Error : ", str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
         comprehend = Comprehend()
+        extracter = KeywordExtractor()
         translate_text = comprehend.translate(product.name)
                 
-        types = ['ORGANIZATION', 'COMMERCIAL_ITEM', 'QUANTITY', 'OTHER']
+        types = ['ORGANIZATION', 'COMMERCIAL_ITEM', 'QUANTITY', 'OTHER', 'FASHION', 'HOME', 
+                'SPORTS', 'ELECTRONICS', 'BEAUTY', 'AUTOMOBILE', 'EXTRA', 'FEATURE']
         keyword_list = []
-
+        category_list = [['0'],
+                        ['3, 200000345, 200000343, 200000297, 201768104, 200574005, 200165144'],
+                        ['6, 13, 15, 1503, 39'],
+                        ['7, 44, 502, 509'],
+                        ['66'],
+                        ['18'],
+                        ['34'],
+                        ['30, 21, 26, 36, 1420, 320']]
+        
         keyword = comprehend.entities1(translate_text)
 
         #commercial_item_texts = extract_commercial_item_text(keyword)
         #keyword_list.append(commercial_item_texts)
 
         if 'COMMERCIAL_ITEM' not in [entity['Type'] for entity in keyword['Entities']]:
-            return Response({"error": "No commercial item found"}, status=status.HTTP_400_BAD_REQUEST)
+            gpt_keyword = extracter.extract_keywords(translate_text)
+            # JSON 문자열을 파이썬 딕셔너리로 로드
+            data_dict = json.loads(gpt_keyword)
 
-        for entity in keyword.get('Entities', []):
-            if entity.get('Type') in types:
-                keyword_list.append(entity.get('Text'))
+            # KEYWORDS 필드에 접근 
+            keyword_list = data_dict["KEYWORDS"]
+            if data_dict["CATEGORIES"].get("FASHION") is not None:
+                category_id = 1
+            elif data_dict["CATEGORIES"].get("HOME") is not None:
+                category_id = 2
+            elif data_dict["CATEGORIES"].get("ELECTRONICS") is not None:
+                category_id = 3
+            elif data_dict["CATEGORIES"].get("BEAUTY") is not None:
+                category_id = 4
+            elif data_dict["CATEGORIES"].get("SPORTS") is not None:
+                category_id = 5
+            elif data_dict["CATEGORIES"].get("AUTOMOBILE") is not None:
+                category_id = 6
+            elif data_dict["CATEGORIES"].get("EXTRA") is not None:
+                category_id = 7        
 
+        else:
+            for entity in keyword.get('Entities', []):
+                if entity.get('Type') in types:
+                    keyword_list.append(entity.get('Text'))
+
+            category_id = 0
         
         if isinstance(keyword_list, list):
             keyword_str = ', '.join(keyword_list)
 
-        category_id = 0
-        '''
-        product_data = {
-            "search_url":search_url,
-            "name":product.name,
-            "keyword": keyword_str,
-            "category_id": category_id,
-            "price":product.price,
-        }
-        #product.save()
-        '''
-        product.category_id = category_id
+        product.category_id = category_list[category_id]
         product.keyword = keyword_str
         product.save()
 
         serializer = KeywordsSerializer(product)
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-        #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
